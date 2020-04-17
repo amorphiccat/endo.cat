9fa8cf497f07c50d243bdfdb852052fff900ce55
cata <amorphiccat@gmail.com>
2020-04-17 16:22:56 +0900

    fix build problem

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,10 @@ const path = require(`path`)
 const _ = require("lodash")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+const sharp = require("sharp")
+sharp.simd(false)
+sharp.cache(false)
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
